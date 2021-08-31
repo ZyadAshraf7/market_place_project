ea7ed6f71f25a13d0c136d8ccb5af4780f926084
ZyadAshraf7 <zyadharfosh88@gmail.com>
2021-08-31 21:01:09 +0200

    Add project files.

---
+++ market_place_project/Cart.cpp
@@ -0,0 +1,13 @@
+//#include "Cart.h"
+//#include "Product.h"
+//
+//Cart::Cart() {
+//
+//}
+//
+//Cart::Cart(products, userId) {
+//	Cart(products, userId) {
+//		this->products = products;
+//		this->userId = userId;
+//	}
+//}
---
+++ market_place_project/Cart.h
@@ -0,0 +1,16 @@
+//#pragma once
+//#include "Person.h"
+//#include "Product.h"
+//#include "vector"
+//#include <String>
+//
+//class Cart {
+//	vector<Product>products;
+//	int userId;
+//	Cart();
+//	Cart(products, userId);
+//
+//	
+//
+//};
+
